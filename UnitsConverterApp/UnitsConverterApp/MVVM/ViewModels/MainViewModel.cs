ceaee86a1a609e6736219a7f70086546bf4968bb
MikolajKos <niko.kosiorek@gmail.com>
2022-11-15 17:32:34 +0100

    Design almost done!

---
+++ UnitsConverterApp/UnitsConverterApp/MVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitsConverterApp.MVVM.ViewModels
+{
+    public class MainViewModel
+    {
+        TestModel model = new TestModel();
+
+        List<string> myList = new List<string>();
+
+        public List<string> ModelDataGrid
+        {
+            get
+            {
+                myList.Add("Mikołaj");
+                myList.Add("Mikołaj");
+                myList.Add("Mikołaj");
+                myList.Add("Mikołaj");
+                return myList;
+            }
+        }
+    }
+}
