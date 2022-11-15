e9582697e4d53cb23ff1ce7907c1765e5700f43f
MikolajKos <niko.kosiorek@gmail.com>
2022-11-15 17:55:18 +0100

    UI Design Done!

--- UnitsConverterApp/UnitsConverterApp/MVVM/ViewModels/MainViewModel.cs
+++ UnitsConverterApp/UnitsConverterApp/MVVM/ViewModels/MainViewModel.cs
@@ -8,20 +8,6 @@ namespace UnitsConverterApp.MVVM.ViewModels
 {
     public class MainViewModel
     {
-        TestModel model = new TestModel();
 
-        List<string> myList = new List<string>();
-
-        public List<string> ModelDataGrid
-        {
-            get
-            {
-                myList.Add("Mikołaj");
-                myList.Add("Mikołaj");
-                myList.Add("Mikołaj");
-                myList.Add("Mikołaj");
-                return myList;
-            }
-        }
     }
 }
